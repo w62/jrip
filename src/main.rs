1cbe54feba52e4d10bd69bac42e86bedd1d0d657
william <william@freepackets.net>
2025-07-01 01:17:51 +0800

    Exit button
    The exit button can properly quit the app.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,35 @@
-use iced::{widget::text, Element};
+use iced::{
+    widget::{button, column, text}, 
+    window, Element, Task,
+};
 
 
 #[derive(Debug, Default)]
 struct AppState {
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 enum Message {
     Exit
 }
 
-fn update (state: &mut AppState, message:Message){
+fn update (state: &mut AppState, message:Message) -> Task<Message>{
+    match message {
+        Message:: Exit => window::get_latest().and_then(window::close),
+    }
 }
 
 fn view(state: &AppState) -> Element<Message>{
-    text("Hello from app").into()
+    column![
+    text("Hello from app").size(24),
+    button(text("Exit").size(24)).on_press(Message::Exit)
+    ]
+    .into()
 
 }
 
 fn main() -> iced::Result{
-    iced::application("Jrip", update, view).run()
+    iced::application("Jrip", update, view)
+        .theme(|_s| iced::Theme::KanagawaDragon)
+        .run()
 }
